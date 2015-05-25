36b09b24bf55d6d6107fcb7ba9d5616a3f65bf11
hrdn <hrdn00@gmail.com>
2015-05-26 03:59:30 +0700

    Read Chunk loop

--- nlp.py
+++ nlp.py
@@ -16,6 +16,14 @@ class Nlp():
         self.recordTrigger.bind("<Button-1>", self.Rekam)
 
     def Rekam(self, event):
+        self.CHUNK = 2048
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 11000
+        self.RECORD_SECONDS = 10
+        self.WAVE_OUTPUT_FILENAME = "output.wav"
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
         self.recordTrigger.bind("<Button-1>", self.Berhenti)
         self.status = True
         t = tr.Thread(target=self._Rekam)
@@ -26,7 +34,10 @@ class Nlp():
         self.recordTrigger.bind("<Button-1>", self.Rekam)
 
     def _Rekam(self):
+        self.frames = []
         while self.status:
+            self.data = self.stream.read(self.CHUNK)
+            self.frames.append(self.data)
             print("Recording")
 
 main = Tk()
