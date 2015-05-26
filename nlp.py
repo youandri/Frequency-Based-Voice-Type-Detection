9adcffbc507a1aa405c047f2addbd49c7e1b4909
hrdn <hrdn00@gmail.com>
2015-05-26 07:04:50 +0700

    Final Source
    - voice type classification
    - frequency acquisition

--- nlp.py
+++ nlp.py
@@ -6,6 +6,7 @@ import wave
 import numpy as np
 from pydub import AudioSegment
 import threading as tr
+from time import sleep
 
 class Nlp():
     def __init__(self, master):
@@ -15,11 +16,16 @@ class Nlp():
         self.recordTrigger = Button(main,image=self.recordBtn)
         self.recordTrigger.image=self.recordBtn
         self.recordTrigger.grid(row=1,column=1)
+        self.stateLabel = Label(main,text="Klik Tombol untuk Record ...",font=("Helvetica", 24),padx=80,bg='#3498db',fg='white')
+        self.stateLabel.grid(row=1,column=2)
+        self.freqLabel = Label(main,text="",font=("Helvetica", 12),padx=20,bg='#3498db',fg='white')
+        self.freqLabel.grid(row=1,column=3)
         self.recordTrigger.bind("<Button-1>", self.Rekam)
 
     def Rekam(self, event):
         self.recordTrigger.config(image=self.stopBtn)
         self.recordTrigger.image=self.stopBtn
+        self.stateLabel.config(text='Recording ...')
         self.CHUNK = 2048
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 1
@@ -67,7 +73,6 @@ class Nlp():
         trimmed_sound.export("output.wav", format="wav")
 
     def play_(self):
-        print('Playing')
         wf = wave.open('output.wav', 'rb')
         chunk = 2048
         swidth = wf.getsampwidth()
@@ -93,10 +98,55 @@ class Nlp():
             stream.write(data)
         stream.close()
         p.terminate()
+        self.olahFrequency(thefreq)
         
+
+    def olahFrequency(self,thefreq):
+        def dist(x1, x2, y1, y2):
+            return np.sqrt(np.sum((x1 - x2) ** 2 + (y1 - y2) ** 2))
+
+        def second_largest(numbers):
+            count = 0
+            m1 = m2 = float('-inf')
+            for x in numbers:
+                count += 1
+                if x > m2:
+                    if x >= m1:
+                        m1, m2 = x, m1
+                    else:
+                        m2 = x
+            return m2 if count >= 2 else None
+
+        def second_smallest(numbers):
+            m1, m2 = float('inf'), float('inf')
+            for x in numbers:
+                if x <= m1:
+                    m1, m2 = x, m1
+                elif x < m2:
+                    m2 = x
+            return m2
+        min_range = [783, 349, 348, 97]
+        max_range = [2086, 887, 1318, 1103]
+        dist_arr = []
+        for x in range(0, 4):
+            temp = dist(min_range[x], min(thefreq), max_range[x], second_largest(thefreq))
+            dist_arr.append(temp)
+            x += 1
+        self.freqLabel.config(text="Min Frequency : " + str("{0:.2f}".format(min(thefreq))) + "\nMax Frequency : " + str("{0:.2f}".format(second_largest(thefreq))))
+        ind = dist_arr.index(min(dist_arr))
+        if ind == 0:
+            self.stateLabel.config(text='Voice Type : Sopran')
+        elif ind == 1:
+            self.stateLabel.config(text='Voice Type : Alto')
+        elif ind == 2:
+            self.stateLabel.config(text='Voice Type : Tenor')
+        elif ind == 3:
+            self.stateLabel.config(text='Voice Type : Bass')
+            
     def process(self):
         self.sr_()
         self.play_()
+        
     def Void(self,event):
         pass
     
@@ -110,6 +160,6 @@ class Nlp():
 main = Tk()
 nlp = Nlp(main)
 main.config(bg='#3498db')
-main.geometry('500x130')
+main.geometry('900x130')
 main.title('Frequency Based Voice Type Detection')
 main.mainloop()
